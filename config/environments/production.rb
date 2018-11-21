f29ace938a5b5e1b866ae040980351f963156494
Supermoox <mooxmk@yahoo.co.uk>
2018-11-21 22:28:09 +0200

    worked on background image again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,14 +28,14 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  #config.assets.compile = false
+  config.assets.compile = false
 
   config.serve_static_assets = true
 
 
 
   config.cache_classes = true
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
 
